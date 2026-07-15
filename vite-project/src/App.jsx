1132e39871f2061b30450bb9484e32598c09462e
Joynobjhorna <joynobjhorna8@gmail.com>
2026-07-15 20:43:08 +0600

    list show

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -24,8 +24,11 @@ function App() {
     const db = getDatabase();
     const getDataref = ref(db, 'TodoName');
     onValue(getDataref, (snapshot) => {
-      const data = snapshot.val();
-      setAllTask(data)
+      let Arr = [];
+      snapshot.forEach((item) => {
+        Arr.push(item.val())
+      });
+      setAllTask(Arr);
     });
   }, [])
 
@@ -36,11 +39,17 @@ function App() {
       <h1 className='text-4xl py-10 mb-30 bg-amber-400 text-white text-center '>Todo Application</h1>
       <form className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4 mx-auto">
         <fieldset className="fieldset">
-          <label className="text-4xl mb-4">Enter your task here!</label>
+          <label className="text-3xl mb-4">Enter your task here!</label>
           <input onChange={(e) => setTask(e.target.value)} type="text" className="input validator" placeholder="Email" required />
           <p className="validator-hint hidden">Required</p>
         </fieldset>
         <button onClick={handleClick} className="btn btn-neutral mt-4" type="submit">submit</button>
+        <ul className = "bg-black text-white p-4">
+          {AllTask.map((item) => (
+            <li className = "text-lg">{item.todoname}</li>
+            
+          ))}
+        </ul>
       </form>
     </>
   )
